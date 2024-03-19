1887b27e6e85b615f0f392a465c2d99f2eff6049
Jitender Kumar Tanwar <jitendertanwar@virtualemployee.com>
2024-03-19 13:43:48 +0530

    after removed key

--- config.js
+++ config.js
@@ -2,6 +2,6 @@
 module.exports = {
     speakerRecognition: {
         endpoint: "westus.api.cognitive.microsoft.com",
-        apiKey: "39f54205ef31498f8ad82a0465de39ac"
+        apiKey: ""
     }
 }
